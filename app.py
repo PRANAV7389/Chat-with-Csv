a2d007273103749c606a89fca23caf28e255adaa
Pranav Sharma <110465335+PRANAV7389@users.noreply.github.com>
2024-11-11 17:59:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,13 +173,7 @@ if menu == "Upload Data":
             st.markdown("### Missing Values Overview")
             missing_values = df.isnull().sum()
             st.write(missing_values[missing_values > 0])
-    if st.button("Clear Uploaded File"):
-        if "uploaded_file" in st.session_state:
-            del st.session_state.uploaded_file  # Properly delete the uploaded file
-        if "df" in st.session_state:
-            del st.session_state.df  # Properly delete the DataFrame
-        st.session_state.clear()  # Clear the session state
-        st.cache_data.clear()  # Clear cached data (if needed)
+    
     else:
         # Information if file was uploaded previously
         if 'uploaded_file' in st.session_state:
